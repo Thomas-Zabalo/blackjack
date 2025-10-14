00695830db0a53619e88852f51ff17679d415fff
Thomas ZABALO <150163380+Thomas-Zabalo@users.noreply.github.com>
2025-10-14 19:46:20 +0200

    Update blackjack.pas

--- Casino/blackjack.pas
+++ Casino/blackjack.pas
@@ -5,7 +5,14 @@ unit BlackJack;
 interface
 
 uses
-  SDL2, SDL2_ttf, SysUtils;
+  SDL2, 
+  SDL2_ttf, 
+  SDL2_image,
+  SysUtils;
+  
+  const
+  WINDOW_WIDTH  = 1920;
+  WINDOW_HEIGHT = 1080;
 
 procedure LancerBlackjack(renderer: PSDL_Renderer; font: PTTF_Font);
 
@@ -36,6 +43,9 @@ const
   CouleursCartes: array[0..3] of string = ('Pique','Trèfle','Carreau','Cœur');
 
 var
+  backgroundTexture: PSDL_Texture;
+  backgroundSurface: PSDL_Surface;
+  
   JeuCartes: array of TCarte;
   IndexCarte: Integer;
   Joueur: TJoueur;
@@ -77,7 +87,7 @@ end;
 procedure InitialiserJeu;
 var
   i, j: Integer;
-begin
+begin 	
   // Créer le jeu de cartes (1 paquet pour simplifier)
   SetLength(JeuCartes, 52);
   for i := 0 to 12 do
@@ -128,6 +138,17 @@ end;
 
 procedure DistribuerCartesInitiales;
 begin
+ 
+  // Vérifier la mise
+  if (MiseActuelle <= 0) or (MiseActuelle > Joueur.Jetons) then
+  begin
+    // Option : afficher un message d'erreur ou ignorer la distribution
+    Exit;
+  end;
+  
+  // Retirer la mise du solde du joueur
+  Joueur.Jetons := Joueur.Jetons - MiseActuelle;
+  
   // Distribuer 2 cartes au joueur
   SetLength(Joueur.Cartes, 2);
   Joueur.Cartes[0] := JeuCartes[IndexCarte]; Inc(IndexCarte);
@@ -175,7 +196,28 @@ begin
 end;
 
 procedure DeterminerResultat;
+var
+  JoueurBlackjack, CroupierBlackjack: Boolean;
 begin
+  JoueurBlackjack := (Length(Joueur.Cartes) = 2) and (Joueur.Score = 21);
+  CroupierBlackjack := (Length(Croupier.Cartes) = 2) and (Croupier.Score = 21);
+
+  // --- Blackjack naturel ---
+  if JoueurBlackjack and not CroupierBlackjack then
+  begin
+    Joueur.Jetons := Joueur.Jetons + Round(MiseActuelle * 2.5); // Paiement 3:2
+    Writeln('Blackjack ! Vous gagnez 3:2');
+  end
+  else if CroupierBlackjack and not JoueurBlackjack then
+  begin
+    Writeln('Le croupier a un Blackjack, vous perdez.');
+  end
+  else if JoueurBlackjack and CroupierBlackjack then
+  begin
+    Joueur.Jetons := Joueur.Jetons + MiseActuelle;
+    Writeln('Égalité : deux Blackjacks.');
+  end;
+  
   if Joueur.Score > 21 then
     // Joueur a dépassé 21, il perd
   else if Croupier.Score > 21 then
@@ -197,6 +239,14 @@ begin
     // Égalité, joueur récupère sa mise
     Joueur.Jetons := Joueur.Jetons + MiseActuelle;
   end;
+end;
+
+function EstBlackjack(joueur: TJoueur): Boolean;
+begin
+  if (Length(joueur.Cartes) = 2) and (CalculerScore(joueur.Cartes) = 21) then
+    Result := True
+  else
+    Result := False;
 end;
 
 procedure NouvelleMain;
@@ -243,20 +293,64 @@ begin
   end;
 end;
 
+procedure DessinerTexteScore(renderer: PSDL_Renderer; font: PTTF_Font; texte: string; x, y: Integer; alignCenter: Boolean);
+var
+  surface: PSDL_Surface;
+  texture: PSDL_Texture;
+  rect, boxRect: TSDL_Rect;
+  color: TSDL_Color;
+begin
+  color.r := 255; color.g := 255; color.b := 255; color.a := 255;
+
+  surface := TTF_RenderText_Blended(font, PChar(texte), color);
+  if surface <> nil then
+  begin
+    texture := SDL_CreateTextureFromSurface(renderer, surface);
+    rect.w := surface^.w;
+    rect.h := surface^.h;
+
+    // --- Carré noir fixe (100x50) ---
+    boxRect.w := 100;
+    boxRect.h := 50;
+    if alignCenter then
+      boxRect.x := x - boxRect.w div 2
+    else
+      boxRect.x := x;
+    boxRect.y := y;
+
+    SDL_SetRenderDrawColor(renderer, 40, 46, 34, 255); // fond noir/vert foncé
+    SDL_RenderFillRect(renderer, @boxRect);
+
+    // --- Centrer le texte dans le carré ---
+    rect.x := boxRect.x + (boxRect.w - rect.w) div 2;
+    rect.y := boxRect.y + (boxRect.h - rect.h) div 2;
+
+    // --- Afficher le texte ---
+    SDL_RenderCopy(renderer, texture, nil, @rect);
+
+    SDL_FreeSurface(surface);
+    SDL_DestroyTexture(texture);
+  end;
+end;
+
 procedure DessinerCarte(renderer: PSDL_Renderer; font: PTTF_Font; carte: TCarte; x, y: Integer; cachee: Boolean);
 var
   rect: TSDL_Rect;
+  surface: PSDL_Surface;
+  texture: PSDL_Texture;
+  color: TSDL_Color;
   texte: string;
+  textRect: TSDL_Rect;
 begin
   // Rectangle de la carte
   rect.x := x;
   rect.y := y;
   rect.w := 80;
   rect.h := 120;
-  
+
   if cachee then
   begin
-    SDL_SetRenderDrawColor(renderer, 0, 0, 139, 255); // Bleu foncé pour carte cachée
+    SDL_SetRenderDrawColor(renderer, 0, 0, 139, 255); // bleu foncé (carte cachée)
     SDL_RenderFillRect(renderer, @rect);
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderDrawRect(renderer, @rect);
@@ -264,31 +358,83 @@ begin
   end
   else
   begin
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // Blanc pour carte visible
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // fond blanc
     SDL_RenderFillRect(renderer, @rect);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // bord noir
     SDL_RenderDrawRect(renderer, @rect);
-    
+
+    // --- texte noir ---
+    color.r := 0;
+    color.g := 0;
+    color.b := 0;
+    color.a := 255;
+
     texte := carte.Valeur + ' ' + Copy(carte.Couleur, 1, 1);
-    DessinerTexte(renderer, font, texte, x + 40, y + 60, True);
+
+    surface := TTF_RenderText_Blended(font, PChar(texte), color);
+    if surface <> nil then
+    begin
+      texture := SDL_CreateTextureFromSurface(renderer, surface);
+      textRect.w := surface^.w;
+      textRect.h := surface^.h;
+      textRect.x := x + (rect.w - textRect.w) div 2;
+      textRect.y := y + (rect.h - textRect.h) div 2;
+
+      SDL_RenderCopy(renderer, texture, nil, @textRect);
+      SDL_FreeSurface(surface);
+      SDL_DestroyTexture(texture);
+    end;
   end;
 end;
+
+
+procedure AfficherCartesCroupier(renderer: PSDL_Renderer; font: PTTF_Font);
+var
+  i, totalCartes, startX: Integer;
+begin
+  totalCartes := Length(Croupier.Cartes);
+  if totalCartes = 0 then Exit; // sécurité
+
+  // Centrer les cartes
+  startX := (WINDOW_WIDTH div 2) - ((totalCartes * 90) div 2);
+
+  for i := 0 to High(Croupier.Cartes) do
+  begin
+    // Si la première carte est cachée, on affiche le dos
+    if (i = 0) and Croupier.PremiereCarteCachee then
+      DessinerCarte(renderer, font, Croupier.Cartes[i], startX + i * 90, 160, True)
+    else
+      DessinerCarte(renderer, font, Croupier.Cartes[i], startX + i * 90, 160, False);
+  end;
+end;
+
+
+procedure AfficherCartesJoueur(renderer: PSDL_Renderer; font: PTTF_Font);
+var
+  i, totalCartes, startX: Integer;
+begin
+  totalCartes := Length(Joueur.Cartes);
+
+  // Largeur totale du groupe de cartes
+  // (écartement 90 px entre chaque carte)
+  startX := (WINDOW_WIDTH div 2) - ((totalCartes * 90) div 2);
+
+  // Dessiner les cartes centrées
+  for i := 0 to High(Joueur.Cartes) do
+    DessinerCarte(renderer, font, Joueur.Cartes[i], startX + i * 90, 560, False);
+end;
+
 
 procedure AfficherJeu(renderer: PSDL_Renderer; font: PTTF_Font);
 var
   i: Integer;
   message, scoreCroupier: string;
-begin
-  // Fond vert de la table
-  SDL_SetRenderDrawColor(renderer, 0, 100, 0, 255);
-  SDL_RenderClear(renderer);
-  
-  // Titre
-  DessinerTexte(renderer, font, 'BLACKJACK', 400, 20, True);
+begin  
+  SDL_RenderCopy(renderer, backgroundTexture, nil, nil);
   
   // Informations joueur
-  DessinerTexte(renderer, font, 'Jetons: ' + IntToStr(Joueur.Jetons), 20, 20, False);
-  DessinerTexte(renderer, font, 'Mise: ' + IntToStr(MiseActuelle), 20, 50, False);
+  DessinerTexte(renderer, font, 'Jetons: ' + IntToStr(Joueur.Jetons), WINDOW_WIDTH div 2 - 150, 1040, False);
+  DessinerTexte(renderer, font, 'Mise: ' + IntToStr(MiseActuelle), WINDOW_WIDTH div 2 + 50, 1040, False);
   
   // Score du croupier
   if Croupier.PremiereCarteCachee then
@@ -296,38 +442,34 @@ begin
   else
     scoreCroupier := IntToStr(Croupier.Score);
   
-  DessinerTexte(renderer, font, 'Croupier - Score: ' + scoreCroupier, 400, 100, True);
+   DessinerTexteScore(renderer, font, scoreCroupier, WINDOW_WIDTH div 2, 100, True);
   
   // Cartes du croupier
-  for i := 0 to High(Croupier.Cartes) do
-  begin
-    if (i = 0) and Croupier.PremiereCarteCachee then
-      DessinerCarte(renderer, font, Croupier.Cartes[i], 300 + i * 90, 130, True)
-    else
-      DessinerCarte(renderer, font, Croupier.Cartes[i], 300 + i * 90, 130, False);
-  end;
+   for i := 0 to High(Croupier.Cartes) do
+	AfficherCartesCroupier(renderer, font);
   
   // Cartes du joueur
-  DessinerTexte(renderer, font, 'Votre main - Score: ' + IntToStr(Joueur.Score), 400, 300, True);
+  DessinerTexteScore(renderer, font, IntToStr(Joueur.Score), WINDOW_WIDTH div 2, 500, True);
   
   for i := 0 to High(Joueur.Cartes) do
-    DessinerCarte(renderer, font, Joueur.Cartes[i], 300 + i * 90, 330, False);
+    AfficherCartesJoueur(renderer, font);
+
   
   // Messages selon l'état du jeu
   case PartieEtat of
     ETAT_MISE:
       begin
-        DessinerTexte(renderer, font, 'MISEZ: 1=10, 2=25, 3=50, 4=100 - ESPACE pour valider', 400, 500, True);
-        DessinerTexte(renderer, font, 'ECHAP pour quitter', 400, 530, True);
+        DessinerTexte(renderer, font, 'MISEZ: 1=10, 2=25, 3=50, 4=100 - ESPACE pour valider', WINDOW_WIDTH div 2, 900, True);
+        DessinerTexte(renderer, font, 'ECHAP pour quitter', WINDOW_WIDTH div 2, 930, True);
       end;
     
     ETAT_JEU:
       begin
-        DessinerTexte(renderer, font, 'H: Hit (Prendre une carte)  S: Stand (Rester)', 400, 500, True);
-        DessinerTexte(renderer, font, 'ECHAP pour quitter', 400, 530, True);
+        DessinerTexte(renderer, font, 'H: Hit (Prendre une carte)  S: Stand (Rester)', WINDOW_WIDTH div 2, 900, True);
+        DessinerTexte(renderer, font, 'ECHAP pour quitter', WINDOW_WIDTH div 2, 930, True);
         
         if Joueur.Score = 21 then
-          DessinerTexte(renderer, font, 'BLACKJACK!', 400, 470, True);
+          DessinerTexte(renderer, font, 'BLACKJACK!', WINDOW_WIDTH div 2, 970, True);
       end;
     
     ETAT_RESULTAT:
@@ -343,8 +485,8 @@ begin
         else
           message := 'EGALITE!';
         
-        DessinerTexte(renderer, font, message, 400, 470, True);
-        DessinerTexte(renderer, font, 'ESPACE pour nouvelle main - ECHAP pour quitter', 400, 500, True);
+        DessinerTexte(renderer, font, message,  WINDOW_WIDTH div 2, 900, True);
+        DessinerTexte(renderer, font, 'ESPACE pour nouvelle main - ECHAP pour quitter',  WINDOW_WIDTH div 2, 930, True);
       end;
   end;
 end;
@@ -353,10 +495,27 @@ procedure LancerBlackjack(renderer: PSDL_Renderer; font: PTTF_Font);
 var
   event: TSDL_Event;
   quit: Boolean;
+    
 begin
   Randomize;
   InitialiserJeu;
   quit := False;
+    
+     // --- Ouvre le background image --- 
+  backgroundSurface := IMG_Load('background_blackjack.png');
+  if backgroundSurface = nil then
+  begin
+    Writeln('Erreur lors du chargement de image :', IMG_GetError);
+    // Nettoyage
+    SDL_DestroyRenderer(renderer);
+    TTF_Quit();
+    SDL_Quit();
+    Halt(1);
+  end;
+  
+  // --- Chargement du background image ---
+  backgroundTexture := SDL_CreateTextureFromSurface(renderer, backgroundSurface);
+  SDL_FreeSurface(backgroundSurface);
   
   while not quit do
   begin
@@ -405,6 +564,9 @@ begin
     SDL_RenderPresent(renderer);
     SDL_Delay(16);
   end;
+  
+   // --- Nettoyage ---
+   SDL_DestroyTexture(backgroundTexture);
 end;
 
 end.
